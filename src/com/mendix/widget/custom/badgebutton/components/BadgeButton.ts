00a25a3cc34eb4282dcdca86b5141709ee72baa7
McDoyen <mrpoloh@yahoo.co.uk>
2017-03-09 11:47:14 +0300

    Add alert unit tests

--- src/com/mendix/widget/custom/badgebutton/components/BadgeButton.ts
+++ src/com/mendix/widget/custom/badgebutton/components/BadgeButton.ts
@@ -1,5 +1,7 @@
-import { Component, DOM, MouseEventHandler, StatelessComponent , createElement } from "react";
+import { Component, DOM, StatelessComponent , createElement } from "react";
+
 import * as classNames from "classnames";
+import { Alert } from "./Alert";
 
 export type BadgeButtonOnclick = "doNothing" | "showPage" | "callMicroflow";
 export type PageSettings = "content" | "popup" | "modal";
@@ -8,38 +10,28 @@ export const ValidationAlert: StatelessComponent<{ message: string }> = (props) 
     DOM.div({ className: "alert alert-danger widget-validation-message" }, props.message);
 
 export interface BadgeButtonProps {
+    alertMessage?: string;
     label?: string;
     badgeValue?: string;
     style?: string;
-    microflow?: {
-        onClickType: BadgeButtonOnclick;
-        microflowProps?: {
-            name: string;
-            guid: string;
-        };
-        pageProps?: {
-            page: string;
-            pageSetting: PageSettings;
-            entity: string;
-            guid: string;
-        };
-    } | any;
+    microflow?: string;
+    onClickAction?: () => void;
     disabled?: string;
 }
 
-export class BadgeButton extends Component<BadgeButtonProps, { alertMessage: string }> {
+export class BadgeButton extends Component<BadgeButtonProps, { alertMessage?: string }> {
     static defaultProps: BadgeButtonProps = { label: "default", style: "default" };
-    private onClickEvent: MouseEventHandler<HTMLDivElement>;
 
     constructor(props: BadgeButtonProps) {
         super(props);
 
-        this.onClickEvent = () => this.handleOnClick(this.props);
-        this.state = { alertMessage: "" };
+        this.state = { alertMessage: props.alertMessage };
     }
 
-    componentDidMount() {
-        this.checkConfig();
+    componentWillReceiveProps(newProps: BadgeButtonProps) {
+        if (newProps.alertMessage !== this.props.alertMessage) {
+            this.setState({ alertMessage: newProps.alertMessage });
+        }
     }
 
     render() {
@@ -49,55 +41,11 @@ export class BadgeButton extends Component<BadgeButtonProps, { alertMessage: str
                     { [`btn-${this.props.style}`]: !!this.props.style }
                 ),
                 disabled: this.props.disabled,
-                onClick: this.onClickEvent
+                onClick: this.props.onClickAction
             },
             DOM.span({ className: "widget-badgebutton-text" }, this.props.label),
             DOM.span({ className: "badge" }, this.props.badgeValue),
-            this.state.alertMessage ? createElement(ValidationAlert, { message: this.state.alertMessage }) : null
+            createElement(Alert, { message: this.state.alertMessage })
         );
-    }
-
-    private checkConfig() {
-        const errorMessage: string[] = [];
-        if (this.props.microflow.onClickType === "callMicroflow"
-            && !this.props.microflow.microflowProps.name) {
-            errorMessage.push("'On click' call a microFlow is set " +
-                "and there is no 'Microflow' Selected in tab Events");
-        }
-        if (this.props.microflow.onClickType === "showPage" && !this.props.microflow.pageProps.page) {
-            errorMessage.push("'On click' Show a page is set and there is no 'Page' Selected in tab 'Events'");
-        }
-        if (errorMessage.length > 0) {
-            errorMessage.unshift("Error in configuration of the BadgeButton widget");
-            this.setState({ alertMessage: errorMessage.join("\n") });
-        }
-    }
-
-    private handleOnClick(props: BadgeButtonProps) {
-        if (props.microflow.onClickType === "callMicroflow"
-            && props.microflow.microflowProps.name && props.microflow.microflowProps.guid) {
-            window.mx.ui.action(props.microflow.microflowProps.name, {
-                error: (error) => {
-                    this.setState({
-                        alertMessage:
-                        `Error while executing microflow: ${props.microflow.microflowProps.name}: ${error.message}`
-                    });
-                },
-                params: {
-                    applyto: "selection",
-                    guids: [ props.microflow.microflowProps.guid ]
-                }
-            });
-        } else if (props.microflow.onClickType === "showPage"
-            && props.microflow.pageProps.page && props.microflow.pageProps.guid) {
-            const context = new mendix.lib.MxContext();
-            context.setTrackId(props.microflow.pageProps.guid);
-            context.setTrackEntity(props.microflow.pageProps.entity);
-
-            window.mx.ui.openForm(props.microflow.pageProps.page, {
-                context,
-                location: props.microflow.pageProps.pageSetting
-            });
-        }
     }
 }
---
+++ src/com/mendix/widget/custom/badgebutton/components/BadgeButtonContainer.ts
@@ -0,0 +1,160 @@
+import { Component, createElement } from "react";
+
+import { BadgeButton, BadgeButtonOnclick, PageSettings } from "./BadgeButton";
+import { Alert } from "./Alert";
+
+interface BadgeButtonContainerProps {
+    contextObject: mendix.lib.MxObject;
+    valueAttribute: string;
+    styleAttribute: string;
+    labelAttribute: string;
+    label: string;
+    badgeClass: string;
+    microflow: string;
+    onClickEvent: BadgeButtonOnclick;
+    page: string;
+    pageSettings: PageSettings;
+}
+
+interface BadgeButtonContainerState {
+    alertMessage?: string;
+    badgeValue: string;
+    label: string;
+    showAlert?: boolean;
+    style: string;
+}
+
+class BadgeButtonContainer extends Component<BadgeButtonContainerProps, BadgeButtonContainerState> {
+    private subscriptionHandles: number[];
+
+    constructor(props: BadgeButtonContainerProps) {
+        super(props);
+
+        this.state = {
+            alertMessage: this.checkConfig(),
+            badgeValue: this.getValue(props.valueAttribute, ""),
+            label: this.getValue(props.labelAttribute, this.props.label),
+            showAlert: !!this.checkConfig(),
+            style: this.getValue(props.styleAttribute, props.badgeClass)
+        };
+        this.subscriptionHandles = [];
+        this.resetSubscriptions(props.contextObject);
+        this.handleOnClick = this.handleOnClick.bind(this);
+    }
+
+    render() {
+        if (this.state.showAlert) {
+            return createElement(Alert, { message: this.state.alertMessage });
+        }
+
+        return createElement(BadgeButton, {
+            alertMessage: this.state.alertMessage,
+            badgeValue: this.state.badgeValue,
+            disabled: this.props.contextObject ? undefined : "disabled",
+            label: this.state.label,
+            microflow: this.props.microflow,
+            onClickAction: this.handleOnClick,
+            style: this.state.style
+
+        });
+    }
+
+    componentWillReceiveProps(newProps: BadgeButtonContainerProps) {
+        this.resetSubscriptions(newProps.contextObject);
+        this.updateValues();
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+
+    private updateValues() {
+        this.setState({
+            badgeValue: this.getValue(this.props.valueAttribute, ""),
+            label: this.getValue(this.props.labelAttribute, "new"),
+            style: this.getValue(this.props.styleAttribute, this.props.badgeClass)
+        });
+    }
+
+    private getValue(attributeName: string, defaultValue: string) {
+        if (this.props.contextObject) {
+            return this.props.contextObject.get(attributeName) as string || defaultValue;
+        }
+        return defaultValue;
+    }
+
+    private resetSubscriptions(contextObject: mendix.lib.MxObject) {
+        this.unsubscribe();
+
+        if (contextObject) {
+            this.subscriptionHandles.push(window.mx.data.subscribe({
+                callback: () => this.updateValues(),
+                guid: contextObject.getGuid()
+            }));
+
+            [ this.props.valueAttribute, this.props.styleAttribute, this.props.labelAttribute ].forEach((attr) =>
+                this.subscriptionHandles.push(window.mx.data.subscribe({
+                    attr,
+                    callback: () => this.updateValues(),
+                    guid: contextObject.getGuid()
+                }))
+            );
+        }
+    }
+
+    private unsubscribe() {
+        if (this.subscriptionHandles) {
+            this.subscriptionHandles.forEach((handle) => window.mx.data.unsubscribe(handle));
+        }
+    }
+
+    private checkConfig(): string {
+        let errorMessage: string = "";
+        if (this.props.onClickEvent === "callMicroflow" && !this.props.microflow) {
+            errorMessage = "on click microflow is required";
+        } else if (this.props.onClickEvent === "showPage" && !this.props.page) {
+            errorMessage = "on click page is required";
+        }
+        if (errorMessage) {
+            errorMessage = `Error in progress circle configuration: ${errorMessage}`;
+        }
+
+        return errorMessage;
+    }
+
+    private handleOnClick() {
+        if (this.props.onClickEvent === "callMicroflow"
+            && this.props.microflow && this.props.contextObject.getGuid()) {
+            window.mx.ui.action(this.props.microflow, {
+                error: (error) => {
+                    this.setState({
+                        alertMessage:
+                        `Error while executing microflow: ${this.props.microflow}: ${error.message}`,
+                        showAlert: false
+                    });
+                },
+                params: {
+                    applyto: "selection",
+                    guids: [ this.props.contextObject.getGuid() ]
+                }
+            });
+        } else if (this.props.onClickEvent === "showPage"
+            && this.props.page && this.props.contextObject.getGuid()) {
+            const context = new mendix.lib.MxContext();
+            context.setTrackId(this.props.contextObject.getGuid());
+            context.setTrackEntity(this.props.contextObject.getEntity());
+
+            window.mx.ui.openForm(this.props.page, {
+                 error: (error) =>
+                    this.setState({
+                        alertMessage: `Error while opening page ${this.props.page}: ${error.message}`,
+                        showAlert: false
+                    }),
+                context,
+                location: this.props.pageSettings
+            });
+        }
+    }
+}
+
+export { BadgeButtonContainer as default };
